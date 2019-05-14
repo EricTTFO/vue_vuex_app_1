feb8144b5016042d0f2546c34e4b2b7c3933fc4d
EricTTFO <34400777+EricTTFO@users.noreply.github.com>
2019-05-14 15:48:25 +0800

    S

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <AddTodo/>
-      <FilterTodos/>
+      <!-- <FilterTodos/> -->
       <Todos/>
     </div>
   </div>
@@ -11,11 +11,12 @@
 <script>
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
-import FilterTodos from "./components/FilterTodos";
+// import FilterTodos from "./components/FilterTodos";
 export default {
   name: "app",
   components: {
-    Todos,AddTodo,FilterTodos
+    Todos,AddTodo,
+    // FilterTodos
   }
 };
 </script>

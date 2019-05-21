319ee358ed907634e2d08551daf81b921db4ee64
EricTTFO <34400777+EricTTFO@users.noreply.github.com>
2019-05-21 10:06:56 +0800

    S

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -6,17 +6,17 @@ const getters = {
 }
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
-    // newTodo: (state, todo) => state.todos.unshift(todo),
-    // removeTodo: (state, id) =>
-    //     (state.todos = state.todos.filter(todo => todo.id !== id)),
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    removeTodo: (state, id) =>
+        (state.todos = state.todos.filter(todo => todo.id !== id)),
 
-    // updateTodo: (state, upTodo) => {
-    //     const index = state.todos.findIndex(todo => todo.id === upTodo.id);
-    //     console.log(index)
-    //     if (index !== -1) {
-    //          state.todos.splice(index,1,upTodo)
-    //     }
-    // }
+    updateTodo: (state, upTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === upTodo.id);
+        console.log(index)
+        if (index !== -1) {
+            state.todos.splice(index, 1, upTodo)
+        }
+    }
 }
 const actions = {
     async fetchTodos({
@@ -26,34 +26,34 @@ const actions = {
         console.log(response);
         commit('setTodos', response.data);
     },
-    // async addTodo({
-    //     commit
-    // }, title) {
-    //     const response = await axios.post(`https://jsonplaceholder.typicode.com/todos`, {
-    //         title,
-    //         completed: false
-    //     });
-    //     commit('newTodo', response.data);
-    // },
-    // async deleteTodo({
-    //     commit
-    // }, id) {
-    //     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    //     commit('removeTodo', id);
-    // },
-    // async filterTodos({
-    //     commit
-    // }, count) {
-    //     console.log(count)
-    //     const response = await axios.get(`http://jsonplaceholder.typicode.com/todos?_limit=${count}`);
-    //     commit('setTodos', response.data);
-    // },
-    // async updateTodo({
-    //     commit
-    // }, upTodo) {
-    //     const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${upTodo.id}`, upTodo);
-    //     commit('updateTodo', response.data);
-    // },
+    async addTodo({
+        commit
+    }, title) {
+        const response = await axios.post(`https://jsonplaceholder.typicode.com/todos`, {
+            title,
+            completed: false
+        });
+        commit('newTodo', response.data);
+    },
+    async deleteTodo({
+        commit
+    }, id) {
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        commit('removeTodo', id);
+    },
+    async filterTodos({
+        commit
+    }, count) {
+        console.log(count)
+        const response = await axios.get(`http://jsonplaceholder.typicode.com/todos?_limit=${count}`);
+        commit('setTodos', response.data);
+    },
+    async updateTodo({
+        commit
+    }, upTodo) {
+        const response = await axios.put(`https://jsonplaceholder.typicode.com/todos/${upTodo.id}`, upTodo);
+        commit('updateTodo', response.data);
+    },
 }
 export default {
     state,
